5d1e55a2b7d62ce86b42407b23acc50620f9e39b
Boo Sparx <tony@Tonys-MacBook-Pro.local>
2017-11-02 14:38:12 -0400

    Burger-3

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,11 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+    } else {
+
+    connection = mysql.createConnection({
 	port: 8889,
     host: '127.0.0.1',
     user: 'root',
